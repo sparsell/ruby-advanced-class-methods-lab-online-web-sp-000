9742fe5e3118cbc83a81fde789a82487f445072b
Sonja Parsell <hello@sparsell.com>
2020-04-23 16:48:44 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +10,62 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+#instantiates and saves the song, and it returns the new song that was created
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+      self.all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+        self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by { |song| song.name }
+  end
+
+
+  def self.new_from_filename(file)
+    rows = file.chop.chop.chop.chop.split(" - ")
+    song = self.new
+    song.name = rows[1]
+    song.artist_name = rows[0]
+    song
+  end
+
+  def self.create_from_filename(file)
+    song = self.new_from_filename(file)
+    song.save
+
+  end
+
+
+  def self.destroy_all
+    @@all.clear
   end
 
 end
